c704d5546acbe074dc22cd7c52340eab502681d1
AnshiMalik <107811970+AnshiMalik@users.noreply.github.com>
2023-02-19 19:48:41 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,6 @@
+function addUser(){
+localStorage.setItem("username",username);
+localStorage.setItem("FirstName",FirstName);
+localStorage.setItem("LastName",LastName);
+window.location= "kwitter_room.html";
+}
